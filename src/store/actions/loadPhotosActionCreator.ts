62e7d5496bbfaf5f90b00f1153a20bbaf84342f4
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-07 11:58:33 +0100

    Test loadPhotosActionCreator

--- src/store/actions/loadPhotosActionCreator.ts
+++ src/store/actions/loadPhotosActionCreator.ts
@@ -5,3 +5,11 @@ export const loadPhotosActionCreator = (photos: Photos): LoadPhotosAction => ({
   type: "loadPhotos",
   payload: photos,
 });
+
+const photos = [
+  { url: "", tags: [], photographer: "" },
+  { url: "", tags: [], photographer: "" },
+  { url: "", tags: [], photographer: "" },
+];
+
+console.log(loadPhotosActionCreator(photos));
