6949fb5045737aa864b88c4c7c25be27e8e986f4
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-07 18:09:26 +0100

    Fix route env variable API key

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -1,24 +1,21 @@
-import { useEffect } from "react";
-import { PhotoData } from "../data/types";
+import { PhotoDataList } from "../data/types";
 
 const useApi = () => {
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(
-        `https://api.unsplash.com/search/photos?page=1&per_page=10&query=magic&client_id=zwvIFWNLhdhYKvJ9iVRXL-W6TU1gcT6TPqzT3YCRyKU`
-      );
+  (async () => {
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&per_page=10&query=magic&client_id=${process.env.REACT_APP_PHOTO_KEY}`
+    );
 
-      const photoApi = (await response.json()) as PhotoData;
+    const photoApi = (await response.json()) as PhotoDataList;
 
-      const photos = photoApi.results.map((result) => ({
-        url: result.urls.regular,
-        tags: result.tags.map((tag) => `#${tag.title}`),
-        photographer: result.user.name,
-      }));
+    const photos = photoApi.results.map((result) => ({
+      url: result.urls.regular,
+      tags: result.tags.map((tag) => `#${tag.title}`),
+      photographer: result.user.name,
+    }));
 
-      return photos;
-    })();
-  });
+    return photos;
+  })();
 };
 
 export default useApi;
