773d99dd2873bd8f00465aa22be1dc0d5c6121fb
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-08 21:28:39 +0100

    Fix misplaced Layout and HomePage components

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import GlobalStyles from "./GlobalStyles";
+import HomePage from "./pages/HomePage/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [{ path: "/", element: <HomePage /> }],
   },
 ]);
 
