8c6dda7d4b6695d55a43c23f978a66c019743d77
RaulMullor <mullor92@gmail.com>
2023-02-08 10:54:01 +0100

    Refactor Photo styles

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -4,12 +4,14 @@ const Photo = (): JSX.Element => {
   return (
     <PhotoStyled className="photo">
       <img
-        className="photo_img"
+        className="photo__img"
         src="./img/regular.jpg"
         alt="Melquiades the mage"
       />
-      <h2 className="photograf">Melquiades Heeringa</h2>
-      <span className="tags">#black #witch #magic</span>
+      <div className="photo__tag">
+        <h2 className="photograf">Melquiades Heeringa</h2>
+        <span className="tags">#black #witch #magic</span>
+      </div>
     </PhotoStyled>
   );
 };
--- src/components/Photo/PhotoStyled.tsx
+++ src/components/Photo/PhotoStyled.tsx
@@ -1,23 +1,38 @@
 import styled from "styled-components";
 
 const PhotoStyled = styled.article`
-  .photo {
+  display: flex;
+  flex-direction: column;
+  height: 355px;
+  width: 312px;
+  border: 1px solid #fff;
+  border-radius: 10% 10% 10% 10%;
+
+  .photo__tag {
     display: flex;
     flex-direction: column;
-    justify-content: center;
     justify-items: center;
+    gap: 10px;
+    padding: 10px 0;
   }
-  .photo_img {
+
+  .photo__img {
     border-radius: 10% 10% 0% 0%;
-    height: 312px;
-    width: 255px;
+    max-height: 72%;
+    max-width: 100%;
     justify-items: center;
+    object-fit: cover;
+    justify-items: flex-start;
   }
+
   .photograf {
     font-size: 24px;
+    padding-left: 12px;
   }
+
   .tags {
     font-size: 18px;
+    padding-left: 12px;
   }
 `;
 
