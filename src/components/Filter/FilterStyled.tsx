d520a61a2a701462345b6b6627f77299e17efe67
RaulMullor <mullor92@gmail.com>
2023-02-08 20:44:02 +0100

    Refactor Filter HomePage and Photo styles

--- src/components/Filter/FilterStyled.tsx
+++ src/components/Filter/FilterStyled.tsx
@@ -6,10 +6,21 @@ const FilterStyled = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+
+    &__button {
+      display: flex;
+      background-color: #eb7d00;
+      width: 80px;
+      border-radius: 15px;
+      gap: 35px;
+      justify-content: center;
+    }
   }
 
   .card_container {
     display: flex;
+    width: 312px;
+    justify-content: space-around;
   }
 `;
 
--- src/components/HomePage/HomePageStyled.tsx
+++ src/components/HomePage/HomePageStyled.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const HomePageStyled = styled.div`
   .title {
     display: flex;
+    padding-left: 20px;
   }
 `;
 
