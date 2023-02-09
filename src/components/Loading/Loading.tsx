e7be6a08ae3c0d80c0208dfdd2b79c67176db9d3
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-09 10:52:11 +0100

    Add styles to Loading component

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,13 +1,14 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import LoadingStyled from "./LoadingStyled";
 
 const Loading = () => {
   return (
-    <>
-      <span>
+    <LoadingStyled>
+      <span className="loading__text">
         Casting...
         <i className="loading__icon fa-solid fa-wand-magic-sparkles"></i>
       </span>
-    </>
+    </LoadingStyled>
   );
 };
 
