33ccc9ce54b49cfcab7db10091c3223ca754bb40
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-09 11:07:33 +0100

    Refactor test Filter component

--- src/components/Filter/Filter.test.tsx
+++ src/components/Filter/Filter.test.tsx
@@ -12,5 +12,35 @@ describe("Given a Filter Component", () => {
 
       expect(buttons).toHaveLength(numberOfButtons);
     });
+
+    test("Then it should show a button to filter photos of castles", () => {
+      const labelText = /castle/i;
+
+      render(<Filter />);
+
+      const castleFilterButton = screen.getByLabelText(labelText);
+
+      expect(castleFilterButton).toBeInTheDocument();
+    });
+
+    test("Then it should show a button to filter photos of dragons", () => {
+      const labelText = /dragon/i;
+
+      render(<Filter />);
+
+      const dragonFilterButton = screen.getByLabelText(labelText);
+
+      expect(dragonFilterButton).toBeInTheDocument();
+    });
+
+    test("Then it should show a button to filter photos of spells", () => {
+      const labelText = /spells/i;
+
+      render(<Filter />);
+
+      const spellsFilterButton = screen.getByLabelText(labelText);
+
+      expect(spellsFilterButton).toBeInTheDocument();
+    });
   });
 });
