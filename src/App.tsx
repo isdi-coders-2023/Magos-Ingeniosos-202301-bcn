b9859ef2e2be5b48a3405ab68c3f3a25156b21ba
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-09 21:37:34 +0100

    Erase fragment in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import Layout from "./components/Layout/Layout";
 
 const App = (): JSX.Element => {
-  return (
-    <>
-      <Layout />
-    </>
-  );
+  return <Layout />;
 };
 
 export default App;
