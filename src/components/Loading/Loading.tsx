a36e7b974cbf3f245c643946ef29f0f946dad46c
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-09 10:38:18 +0100

    Create Loading component structure

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,14 @@
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+const Loading = () => {
+  return (
+    <>
+      <span>
+        Casting...
+        <i className="loading__icon fa-solid fa-wand-magic-sparkles"></i>
+      </span>
+    </>
+  );
+};
+
+export default Loading;
