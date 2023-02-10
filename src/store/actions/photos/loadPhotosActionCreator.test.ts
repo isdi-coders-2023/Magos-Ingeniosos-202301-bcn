265ccaadea9604cbdbd4a9d79c633ca640a12439
Róman Demchuk <roman.demchuk@gmail.com>
2023-02-10 20:05:50 +0100

    Add uiReducer and uiActionCreator tests

--- src/store/actions/photos/loadPhotosActionCreator.test.ts
+++ src/store/actions/photos/loadPhotosActionCreator.test.ts
@@ -1,26 +1,20 @@
 import { loadPhotosActionCreator } from "./loadPhotosActionCreator";
 
+const photos = [
+  { id: "", alt: "", url: "", tags: [], photographer: "" },
+  { id: "", alt: "", url: "", tags: [], photographer: "" },
+  { id: "", alt: "", url: "", tags: [], photographer: "" },
+];
+
 describe("Given a loadPhotosActionCreator function", () => {
   describe("When it receives an array of photos data", () => {
     test("Then it should return an action object with property 'type'", () => {
-      const photos = [
-        { id: "", alt: "", url: "", tags: [], photographer: "" },
-        { id: "", alt: "", url: "", tags: [], photographer: "" },
-        { id: "", alt: "", url: "", tags: [], photographer: "" },
-      ];
-
       const result = loadPhotosActionCreator(photos);
 
       expect(result).toHaveProperty("type");
     });
 
     test("Then it should return an action object with property 'payload'", () => {
-      const photos = [
-        { id: "", alt: "", url: "", tags: [], photographer: "" },
-        { id: "", alt: "", url: "", tags: [], photographer: "" },
-        { id: "", alt: "", url: "", tags: [], photographer: "" },
-      ];
-
       const result = loadPhotosActionCreator(photos);
 
       expect(result).toHaveProperty("payload");
