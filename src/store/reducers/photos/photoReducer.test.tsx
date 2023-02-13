ae5153007dfb7b46afaff79c23081d00172fec55
Ana Coronel <a9nna.coronel@gmail.com>
2023-02-13 16:28:24 +0100

    Delete default type from PhotoActionType

--- src/store/reducers/photos/photoReducer.test.tsx
+++ src/store/reducers/photos/photoReducer.test.tsx
@@ -15,7 +15,10 @@ const photo = {
 describe("Given a photoReducer function", () => {
   describe("When it receives data of one photo and no action", () => {
     test("Then it should return the same photo data unchanged", () => {
-      const defaultAction = { type: PhotoActionType.default, payload: [] };
+      const defaultAction = {
+        type: "" as unknown as PhotoActionType,
+        payload: [],
+      };
 
       const result = photoReducer(photo, defaultAction);
 
